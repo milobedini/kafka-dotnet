808712c5918a7ae45c5ca8d49f934fd31ec8b268
Milo Bedini <milo.bedini@dae.mn>
2022-11-28 17:50:26 +0000

    removed comments

--- producer/producer.cs
+++ producer/producer.cs
@@ -10,19 +10,12 @@ var config = new ProducerConfig
     BootstrapServers = "localhost:9092",
 };
 
-string topic = "Users";
+const string topic = "Users";
 
-// Null as the key and string as the value
 using var producer = new ProducerBuilder<Null, string>(config).Build();
-
-// Just need to adapt consumer to act on a
 
 try
 {
-    // string? firstName;
-    // while ((firstName = Console.ReadLine()) != null)
-    // Enter name into the terminal, which will be serialised and produce a new user object with the inputted first name.
-
 
     int objId = 0;
     while (objId <= 10)
@@ -36,7 +29,6 @@ try
             )
         }
         );
-        Console.WriteLine(response.Value);
         Console.WriteLine($"Sent {response.Value.Length} bytes to {topic} with the User ID of {objId}");
         Thread.Sleep(3000);
         objId += 1;
